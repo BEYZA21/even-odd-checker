03114b6e4b2e89185cacb7f166ce8d1e9439dfe0
BEYZA21 <89150497+BEYZA21@users.noreply.github.com>
2025-03-07 22:25:58 +0300

    Muazzez Beyza ABHAZA

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        return f"{number} is even."
+    else:
+        return f"{number} is odd."
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    print(check_even_odd(num))
